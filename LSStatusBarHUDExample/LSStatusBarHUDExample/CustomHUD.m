f6bd6d96e5e8bbe9d706bf6493b1f1fccabba2a6
ls <itiapp@163.com>
2016-12-07 19:38:49 +0800

    提交

--- LSStatusBarHUDExample/LSStatusBarHUDExample/CustomHUD.m
+++ LSStatusBarHUDExample/LSStatusBarHUDExample/CustomHUD.m
@@ -14,23 +14,25 @@
 @implementation CustomHUD
 
     
-    +(void)showMessage:(NSString *)message
-    {
-        
-       NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:message color:[UIColor redColor] font:[UIFont systemFontOfSize:20]];
-        
-        [LSStatusBarHUD showMessage:a backgroundColor:[UIColor colorWithRed:0/255.0 green:121/255.0 blue:195/255.0 alpha:1]];
-    }
-    
-    +(void)showLoading:(NSString *)loading
++(void)showMessage:(NSString *)message
 {
-         NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:loading color:[UIColor blueColor] font:[UIFont systemFontOfSize:20]];
+    
+    NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:message color:[UIColor redColor] font:[UIFont systemFontOfSize:20]];
+    
+    [LSStatusBarHUD showMessage:a backgroundColor:[UIColor colorWithRed:0/255.0 green:121/255.0 blue:195/255.0 alpha:1]];
+}
+
++(void)showLoading:(NSString *)loading
+{
+    NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:loading color:[UIColor blueColor] font:[UIFont systemFontOfSize:20]];
     NSMutableArray *images=[NSMutableArray array];
     for (int i=1; i<=12; i++) {
         [images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"%d.png",i]]];
     }
     [LSStatusBarHUD showLoading:a images:images backgroundColor:[UIColor yellowColor]];
     
-        
-    }
+}
+
+
+
 @end
--- README.md
+++ README.md
@@ -30,6 +30,29 @@
 	{
     	[LSStatusBarHUD hideLoading];    
 	}
+	
+	
+#如果自己的软件有自己的统一风格最好自定义个类，然后在类方法里实现自己的风格这样每次自己调用的时候就可以非常方便
+##自定义映客显示效果	 
+###这里自定义类CustomHUD 
+	+(void)showMessage:(NSString *)message
+	{
+    
+    NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:message color:	[UIColor redColor] font:[UIFont systemFontOfSize:20]];    
+    [LSStatusBarHUD showMessage:a backgroundColor:[UIColor colorWithRed:0/255.0 green:121/255.0 blue:195/255.0 alpha:1]];
+	}
+##自定义qq音乐加载效果
+	+(void)showLoading:(NSString *)loading
+	{
+    	NSMutableAttributedString *a= [LSStatusBarHUD createAttributedText:loading color:[UIColor blueColor] font:[UIFont systemFontOfSize:20]];
+    NSMutableArray *images=[NSMutableArray array];
+    	for (int i=1; i<=12; i++) 
+    	{
+      	  [images addObject:[UIImage imageNamed:[NSString stringWithFormat:@"%d.png",i]]];
+      	  	 }
+    	[LSStatusBarHUD showLoading:a images:images backgroundColor:[UIColor yellowColor]];
+	}
+
 ##自定义映客效果使用方法
 	- (IBAction)customTip:(id)sender {
     	[CustomHUD showMessage:@"哈哈哈哈哈哈哈哈"];
